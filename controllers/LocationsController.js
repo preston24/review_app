ccd0d907246e39e7dbb1473c0e61da71705a7993
Preston Bayle <prestonbayle@gmail.com>
2018-03-29 18:24:37 -0600

    ability to delete a location

--- controllers/LocationsController.js
+++ controllers/LocationsController.js
@@ -12,9 +12,20 @@ module.exports = {
     const user_id = req.user.id;
 
     let locationObj = { ...req.body, user_id };
-    
+
     db.creat_location(locationObj).then(results => {
       res.send(results[0]);
+    });
+  },
+
+  delete: (req, res) => {
+    const db = req.app.get('db')
+    const user_id = req.user.id;
+
+    const { id: location_id } = req.params
+
+    db.delete_location({ location_id, user_id }).then(locations => {
+      res.send(locations);
     });
   }
 };
